2eafe382589cee04f822ed4d0e7ae9a0dce10220
Edward Nimocks <enimocks@gmail.com>
2018-05-18 20:02:15 -0400

    bootstrap HTML structure and form control script mostly finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,9 +6,15 @@ const friends = require("../data/friends");
 
 module.exports = function(app) {
 
+  app.get('api/friends', function (res, req) {
 
+    res.json(friends);
+  });
 
+  app.post('api/friends', function (res,req) {
 
+    
+  });
 
   
 }
